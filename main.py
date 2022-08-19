bfa90891a667a901eb990d00f2a151c3c73158a5
luacarol <luanacaroliny07@gmail.com>
2022-08-19 08:16:02 -0300

    Function sentiment analysis added

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask("my_app")
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "My sentiment analysis app."
 
-app.run()
+@app.route('/sentiment/<phase>')
+def sentiment(phase):
+    tb = TextBlob(phase)
+    tb_en = tb.translate(from_lang='pt_br', to='en')
+    polarity = tb_en.sentiment.polarity
+    return "Polarity: {}".format(polarity)
+
+app.run(debug=True)
